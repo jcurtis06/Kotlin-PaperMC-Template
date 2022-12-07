b207e3ad2ff52b5ec2981663ce18136202db5a68
Jonathan Curtis <77545656+jcurtis06@users.noreply.github.com>
2022-12-07 11:17:29 -0500

    Init

---
+++ src/main/kotlin/io/jcurtis/kotlinexample/KotlinPaper.kt
@@ -0,0 +1,17 @@
+package io.jcurtis.kotlinexample
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class KotlinPaper: JavaPlugin() {
+    override fun onEnable() {
+        // Pair is part of the Kotlin standard library
+        // this is a test to see if it works properly on the server
+        val testPair = Pair("Hello", "World")
+
+        logger.info("KotlinPaper has been enabled!")
+
+        // these lines test to make sure that the Kotlin standard library is working
+        logger.info("${testPair.first} From KotlinPaper")
+        logger.info("${testPair.second} From KotlinPaper")
+    }
+}
